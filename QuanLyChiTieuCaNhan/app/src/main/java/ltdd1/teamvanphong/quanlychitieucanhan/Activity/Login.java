467036a2b00706c89bce9586d92de01ca1ef6089
Cao Phan Khải <caophankhai123@gmail.com>
2024-07-12 18:52:00 +0700

    update

---
+++ QuanLyChiTieuCaNhan/app/src/main/java/ltdd1/teamvanphong/quanlychitieucanhan/Activity/Login.java
@@ -0,0 +1,20 @@
+package ltdd1.teamvanphong.quanlychitieucanhan.Activity;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import ltdd1.teamvanphong.quanlychitieucanhan.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
