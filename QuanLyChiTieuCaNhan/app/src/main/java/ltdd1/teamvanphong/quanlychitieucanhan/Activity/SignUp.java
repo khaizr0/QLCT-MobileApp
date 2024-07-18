0d57b930284b8430fc9f205b5d61be971c784d45
Cao Phan Khải <caophankhai123@gmail.com>
2024-07-19 04:01:58 +0700

    done SignUp (with default category add)

--- QuanLyChiTieuCaNhan/app/src/main/java/ltdd1/teamvanphong/quanlychitieucanhan/Activity/SignUp.java
+++ QuanLyChiTieuCaNhan/app/src/main/java/ltdd1/teamvanphong/quanlychitieucanhan/Activity/SignUp.java
@@ -1,11 +1,9 @@
 package ltdd1.teamvanphong.quanlychitieucanhan.Activity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,9 +13,11 @@ import ltdd1.teamvanphong.quanlychitieucanhan.R;
 
 public class SignUp extends AppCompatActivity {
 
-    private EditText editTextUsername, editTextPassword, editTextEmail, editTextSDT;
+    private EditText editTextUsername;
+    private EditText editTextPassword;
+    private EditText editTextEmail;
+    private EditText editTextSDT;
     private Button buttonSignUp;
-    private TextView textViewLogin,textViewForgotPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,40 +29,21 @@ public class SignUp extends AppCompatActivity {
         editTextEmail = findViewById(R.id.editTextEmail);
         editTextSDT = findViewById(R.id.editTextSDT);
         buttonSignUp = findViewById(R.id.buttonLogin);
-        textViewLogin = findViewById(R.id.textViewLogin);
-        textViewForgotPassword = findViewById(R.id.textViewForgotPassword);
 
-        buttonSignUp.setOnClickListener(view -> {
-            String username = editTextUsername.getText().toString();
-            String password = editTextPassword.getText().toString();
-            String email = editTextEmail.getText().toString();
-            String phone = editTextSDT.getText().toString();
-
-            int gender = 1;
-
-            boolean isRegistered = UserModel.registerUser(this, username, email, password, gender, phone);
-
-            if (isRegistered) {
-                Toast.makeText(this, "Đăng ký thành công", Toast.LENGTH_SHORT).show();
-                finish();
-            } else {
-                Toast.makeText(this, "Đăng ký thất bại", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        textViewLogin.setOnClickListener(new View.OnClickListener() {
+        buttonSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(SignUp.this, Login.class);
-                startActivity(intent);
-            }
-        });
+            public void onClick(View v) {
+                String username = editTextUsername.getText().toString();
+                String password = editTextPassword.getText().toString();
+                String email = editTextEmail.getText().toString();
+                String phone = editTextSDT.getText().toString();
 
-        textViewForgotPassword.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(SignUp.this, checkInfo_resetPass.class);
-                startActivity(intent);
+                if (UserModel.registerUser(SignUp.this, username, email, password, 1, phone)) { // Assuming gender as 1
+                    Toast.makeText(SignUp.this, "Đăng ký thành công!", Toast.LENGTH_SHORT).show();
+                    finish(); // Close the sign-up activity
+                } else {
+                    Toast.makeText(SignUp.this, "Đăng ký thất bại. Vui lòng thử lại!", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
